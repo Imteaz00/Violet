6c43e4fd76f380850df86bbe5a19d0a8ebb8d58b
Imteaz00 <imteaz969@gmail.com>
2026-01-01 23:01:28 +0600

    backend 0.1

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { ENV } from "./src/config/env";
 
 export default defineConfig({
-  schema: "./src/config/index.ts",
+  schema: "./src/config/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: ENV.database_url!,
