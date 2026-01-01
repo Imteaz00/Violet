b0fc4a534153f52340d3b9f4b63fc7354b6d550e
Imteaz00 <imteaz969@gmail.com>
2026-01-01 13:58:19 +0600

    queries for user and products, and formatting

---
+++ backend/src/db/types.ts
@@ -0,0 +1,10 @@
+import { productImages, products, users } from "./schema.js";
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
+
+export type Product = typeof products.$inferSelect;
+export type NewProduct = typeof products.$inferInsert;
+
+export type ProductImage = typeof productImages.$inferSelect;
+export type NewProductImage = typeof productImages.$inferInsert;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,22 @@
-import express from "express"
-import {ENV} from "./config/env.ts"
-import { clerkMiddleware } from '@clerk/express'
-import cors from "cors"
+import express from "express";
+import { ENV } from "./config/env.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({origin: ENV.frontend_url}))
-app.use(clerkMiddleware())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(cors({ origin: ENV.frontend_url }));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/api", (req, res) => {
+  res.json({
+    message: "server",
+    endpoints: {},
+  });
+});
 
-app.get("/api", (req,res) => {
-    res.json({
-        message: "server",
-        endpoints: {}
-    })
-})
-
-
-app.listen(ENV.port, () => console.log(`Server is running on port ${ENV.port}`))
+app.listen(ENV.port, () =>
+  console.log(`Server is running on port ${ENV.port}`)
+);
